90f62dd8aac6d18c2017bf7b408cf45e2fa9dac0
VicentAG <vialga03@xuqueralumnat.es>
2023-12-01 09:26:36 +0100

    Archivos

---
+++ OtraPureba.java
@@ -0,0 +1,7 @@
+package com.dam.prueba;
+
+public class OtraPureba {
+    public static void main(String[] args) {
+        System.out.println("Esto es otra prueba");
+    }
+}
---
+++ prueba.java
@@ -0,0 +1,7 @@
+package com.dam.prueba;
+
+public class prueba {
+    public static void main(String[] args) {
+        System.out.println("Hola");
+    }
+}
